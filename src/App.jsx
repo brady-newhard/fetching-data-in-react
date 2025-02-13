514e779365ea7ef4ff2cad0537d6b1beb1545a2c
Brady Newhard <brady.russell@gmail.com>
2025-02-13 00:14:29 -0500

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as weatherService from './services/weatherService';
 import WeatherSearch from './components/WeatherSearch/WeatherSearch';
 import WeatherDetails from './components/WeatherDetails/WeatherDetails';
@@ -18,6 +18,22 @@ const App = () => {
   };
   // The following log should be outside of the fetchData function
   console.log('State:', weather);
+
+  useEffect(() => {
+
+    // Define a fetch function:
+    const fetchDefaultData = async () => {
+      const data = await weatherService.show('New York');
+      const newWeatherState = {
+        location: data.location.name,
+        temperature: data.current.temp_f,
+        condition: data.current.condition.text,
+      };
+      setWeather(newWeatherState);
+    };
+     // Call the fetch function when the page loads:
+     fetchDefaultData();
+  }, []);
 
   return (
     <main>
